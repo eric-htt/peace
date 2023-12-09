e61c97e86f7a9a2e075f70305978cfd61656276a
Eric Purdy <eric.purdy@gmail.com>
2023-12-09 13:19:37 -0800

    updated website content

---
+++ qr_code.py
@@ -0,0 +1,21 @@
+import sys
+
+import qrcode
+
+# Data to be encoded
+data = sys.argv[1]
+
+# Generate QR code
+qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save(sys.argv[2])
+
